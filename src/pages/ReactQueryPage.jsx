e45365101ca6eaff8871ef08017c181af058bb72
haejin <luckyhaejin1126@gmail.com>
2024-02-20 14:40:14 +0900

    디테일 데이터 불러오는 방법

--- src/pages/ReactQueryPage.jsx
+++ src/pages/ReactQueryPage.jsx
@@ -3,20 +3,18 @@ import axios from 'axios';
 import React from 'react'
 
 const ReactQueryPage = () => {
-    const fetchPost = () => {
-        return axios.get('http://localhost:3004/posts');
+    const fetchPost = (queryData) => {
+        console.log("queryData", queryData);
+        const id = queryData.queryKey[1];
+        return axios.get(`http://localhost:3004/posts/${id}`);
     }
     const {isLoading, data ,isError, error, refetch} = useQuery({
-        queryKey:['posts'],
+        queryKey:['posts', 1],
         queryFn: fetchPost,
         retry:1,
         select:(data) => {
             return data.data;
         },
-        // refetchInterval: 3000, //해당 시간마다 api 호출하는거
-        // refetchOnMount: false, //component가 시작될 때 fetch할지말지 여부 (home에서 reactQuery페이지 갔을 때 한번만 부르고 영원히 안부르고 true로 하면 갈 떄 마다 호출됨. 기본값은 true)
-        // refetchOnWindowFocus: true, //해당 화면에 포커스 될 때마다 자동으로 reFresh해주는거(유저에게 매번 새로운 데이터 보여주고 싶을 때 사용)
-        enabled:false,
     });
     console.log('data', data, ', isLoading', isLoading);
     console.log('isError', isError, 'error', error);
@@ -28,9 +26,9 @@ const ReactQueryPage = () => {
     }
     return (
         <div>
-            {data?.map((item, index) => (
+            {/* {data?.map((item, index) => (
                 <div key={data[index].id}>{item.title}</div>
-            ))}
+            ))} */}
             <button onClick={refetch}>post 리스트 다시 들고오기</button>
         </div>
     );
